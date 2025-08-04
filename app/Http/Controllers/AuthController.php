64214d4ccee4318d96449e6b8ab8cffa42d01e40
tungnguyen <minhtung.uit@gmail.com>
2025-08-04 23:36:52 +0700

    Create API Register User

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+use function Laravel\Prompts\error;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|same:confirmed_password'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()->all()]);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $input['name'] = $user->name;
+        $input['email'] = $user->email;
+        $input['token'] = $user->createToken('app')->plaintextToken;
+
+        return response()->json([$input]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;    
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::apiResource('products', App\Http\Controllers\ProductController::class);
+
+Route::post('register', [AuthController::class, 'register']);
